300893381be65215faea99c1af04a7e9381d9cb1
Shumaila Saeed <shumailasaeeed85@gmail.com>
2021-05-30 20:35:01 +0500

    work Complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var column = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+        SizedBox(height: 50,),
+        Container(
+          width: 200,
+          child: TextField() ,),
+          Container(
+          width: 200,
+          child: TextField() ,),
+          ElevatedButton(onPressed: (){}, child: Text("Login"))
+      ],);
     return MaterialApp(
-      home: (
-        Scaffold(body: Text("Shumaila Saeed"))
-      )
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(appBar: AppBar(
+        title: Center(child: Text("Login page"),),),
+      body: Center(child: column)
+      ,),
+      
     );
   }
 }
+
